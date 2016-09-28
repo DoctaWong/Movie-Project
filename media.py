074d6363a22f302f30c0ee0f8e38222a4d54fc13
Wilson Wong <williewonga@gmail.com>
2016-09-27 20:17:21 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,8 +1,11 @@
 import webbrowser
 
 class Movie():
+    '''This class stores information specified in the __init__ function'''
     
     def __init__(self, title, genre, length, imdb_rating, poster_image, trailer_youtube, release_date):
+        '''stores the following information about each movie'''
+        
         self.title = title
         self.genre = genre
         self.length = length
@@ -12,6 +15,6 @@ class Movie():
         self.release_date = release_date
 
     def show_trailer(self):
-        '''opens a web browser with the link specified in trailer_url'''
+        """opens a web browser with the link specified in trailer_url"""
     
         webbrowser.open(self.trailer_url)
