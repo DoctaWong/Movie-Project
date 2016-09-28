faa82015e381829775ea875d56d55b0fb7253b3c
Wilson Wong <williewonga@gmail.com>
2016-09-28 01:05:41 -0500

    Update media.py

--- media.py
+++ media.py
@@ -8,15 +8,15 @@ class Movie():
                  trailer_youtube, release_date):
         '''stores the following information about each movie'''
 
-    self.title = title
-    self.genre = genre
-    self.length = length
-    self.imdb_rating = imdb_rating
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
-    self.release_date = release_date
+        self.title = title
+        self.genre = genre
+        self.length = length
+        self.imdb_rating = imdb_rating
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.release_date = release_date
 
     def show_trailer(self):
         """opens a web browser with the link specified in trailer_url"""
 
-webbrowser.open(self.trailer_url)
+        webbrowser.open(self.trailer_url)
